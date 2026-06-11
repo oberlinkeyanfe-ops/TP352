ce84183f22fd192482e70a4f6931a29198aa49e8
oberlinkeyanfe-ops <oberlin.keyanfe@facsciences-uy1.cm>
2026-06-11 12:50:06 +0100

    Initial commit - Application bancaire

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,23 @@
 -- Insertion des banques initiales
-INSERT INTO banks (name, code, created_at) VALUES ('Banque Nationale', 'BNAT', CURRENT_TIMESTAMP);
-INSERT INTO banks (name, code, created_at) VALUES ('Crédit Agricole', 'CAG', CURRENT_TIMESTAMP);
-INSERT INTO banks (name, code, created_at) VALUES ('Société Générale', 'SG', CURRENT_TIMESTAMP);
-INSERT INTO banks (name, code, created_at) VALUES ('Union Banque Africaine', 'UBA', CURRENT_TIMESTAMP);
-INSERT INTO banks (name, code, created_at) VALUES ('CCA', 'CCA', CURRENT_TIMESTAMP);
+INSERT INTO banks (name, code, created_at) 
+SELECT 'Banque Nationale', 'BNAT', CURRENT_TIMESTAMP
+WHERE NOT EXISTS (SELECT 1 FROM banks WHERE code = 'BNAT');
+
+INSERT INTO banks (name, code, created_at) 
+SELECT 'Crédit Agricole', 'CAG', CURRENT_TIMESTAMP
+WHERE NOT EXISTS (SELECT 1 FROM banks WHERE code = 'CAG');
+
+INSERT INTO banks (name, code, created_at) 
+SELECT 'Société Générale', 'SG', CURRENT_TIMESTAMP
+WHERE NOT EXISTS (SELECT 1 FROM banks WHERE code = 'SG');
+
+INSERT INTO banks (name, code, created_at) 
+SELECT 'Union Banque Africaine', 'UBA', CURRENT_TIMESTAMP
+WHERE NOT EXISTS (SELECT 1 FROM banks WHERE code = 'UBA');
+
+INSERT INTO banks (name, code, created_at) 
+SELECT 'CCA', 'CCA', CURRENT_TIMESTAMP
+WHERE NOT EXISTS (SELECT 1 FROM banks WHERE code = 'CCA');
 
 -- Insertion d'un utilisateur test
 INSERT INTO users (name, email, created_at, updated_at) VALUES ('Jean', 'jean@email.com', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
